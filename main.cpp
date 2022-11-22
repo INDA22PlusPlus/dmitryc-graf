f876644597df45fae469f71416c928bc92643d05
Dmitry Chirin <chirin.dima@gmail.com>
2022-11-22 21:37:47 +0100

    add construction of all graphs

--- main.cpp
+++ main.cpp
@@ -18,30 +18,39 @@ public:
     vector<Edge> edges;
 };
 
+//class Graph {
+//public:
+//    Vertex verices[];
+//};
+
 int main() {
 //    std::priority_queue<Edge> pq;
-    long long vertices, edges, amount_of_quires, starting_vertex;
-    cin >> vertices >> edges >> amount_of_quires >> starting_vertex;
 
-    Vertex graph [vertices];
-    int quires [amount_of_quires];
+    while (true) {
+        long long vertices, edges, amount_of_quires, starting_vertex;
+        cin >> vertices >> edges >> amount_of_quires >> starting_vertex;
+        if (vertices == 0 and edges == 0 and amount_of_quires == 0 and starting_vertex == 0) {
+            break;
+        }
 
-    // TODO: Loop without i
-    for (int i = 0; i < edges; i++) {
-        long long vertex_from, vertex_to, weight;
-        cin >> vertex_from >> vertex_to >> weight;
-        graph[vertex_from].edges.emplace_back(vertex_to, weight);
+        Vertex graph [vertices];
+        int quires [amount_of_quires];
+
+        // TODO: Loop without i
+        for (int i = 0; i < edges; i++) {
+            long long vertex_from, vertex_to, weight;
+            cin >> vertex_from >> vertex_to >> weight;
+            graph[vertex_from].edges.emplace_back(vertex_to, weight);
 //        Edge last = graph[vertex_from].edges.back();
 //        cout << endl << last.vertex << last.weight;
+        }
+
+        // TODO: Loop without i
+        for (int i = 0; i < amount_of_quires; i++) {
+            cin >> quires[i];
+            cout << quires[i] << endl;
+        }
     }
-
-    // TODO: Loop without i
-    for (int i = 0; i < amount_of_quires; i++) {
-        cin >> quires[i];
-        cout << quires[i] << endl;
-    }
-
-
 
 //    cout << endl << "Hello, World!" << endl;
     return 0;
