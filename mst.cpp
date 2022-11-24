c678490b7bd7f5cdcb963ca2d8c2c69af3ded2f6
Dmitry Chirin <chirin.dima@gmail.com>
2022-11-24 22:46:21 +0100

    another attempt, wrong mst

--- mst.cpp
+++ mst.cpp
@@ -1,167 +1,155 @@
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//
+//using namespace std;
+//
+//long long INFP = 100000005LL;
+//long long INFN = -100000005LL;
+//
+//
+//int main() {
+//    int vertices_amount, edges_amount;
+//    cin >> vertices_amount >> edges_amount;
+//
+//    while (!(vertices_amount == 0 and edges_amount == 0)) {
+//        // Getting the input, storing it to graph
+//        vector<vector<pair<int, int>>> graph(vertices_amount);
+//        for (int edge = 0; edge < edges_amount; edge++) {
+//            int from, to, weight;
+//            cin >> from >> to >> weight;
+//            graph[from].push_back({to, weight});
+//        }
+//
+//        // Prim's Algorithm
+//
+////        while (!pq.empty()) {
+////            // TODO: Fix this, had problems with references, solved with sketchy id system so far
+////            long long vertex_id = pq.top().id;
+////            pq.pop();
+////            Vertex& vertex = vertices[vertex_id];
+////
+////            for (Edge edge: vertex.edges) {
+////                long long new_distance = vertex.distance + edge.weight;
+////                Vertex& adjacent_vertex = vertices[edge.vertex];
+////
+////                if (new_distance < adjacent_vertex.distance) {
+////                    pq.push(adjacent_vertex);
+////                    adjacent_vertex.distance = new_distance;
+////                }
+////            }
+////        }
+//    }
+//
+//}
+
+// Prim's Algorithm in C++
+
+#include <cstring>
 #include <iostream>
-#include <queue>
-#include <climits>
 using namespace std;
 
-class Edge {
-public:
-    long long vertex;
-    long long weight;
+int INF = 1000005;
 
-    Edge(long long vertex, long long weight) {
-        this->vertex = vertex;
-        this->weight = weight;
-    }
-};
+// create a 2d array of size 5x5
+//for adjacency matrix to represent graph
 
-struct CompareEdges {
-    bool operator()(Edge& v1, Edge& v2)
-    {
-        return v1.weight < v2.weight;
-    }
-};
-
-class Vertex {
-public:
-    priority_queue<Edge, vector<Edge>, CompareEdges> edges;
-    long long distance = LLONG_MAX;
-    long long id;
-
-    Vertex() {
-        this->id = 0;
-    }
-
-    explicit Vertex(long long id) {
-        this->id = id;
-    }
-};
-
-struct CompareDistances {
-    bool operator()(Vertex& v1, Vertex& v2)
-    {
-        return v1.distance < v2.distance;
-    }
-};
-
-// TODO: Fix vectors to arrays
-class Graph {
-public:
-    vector<Vertex> vertices;
-
-    explicit Graph(long long vertices_amount) {
-        this->vertices = vector<Vertex>(vertices_amount);
-
-        // TODO: Fix this
-        for (long long i = 0; i < vertices_amount; i++) {
-            this->vertices[i].id = i;
-        }
-    }
-
-    // Adds edges from cin input
-    void add_edges_from_input(long long edges, bool print=false) {
-        // TODO: Loop without i
-        for (int i = 0; i < edges; i++) {
-            long long vertex_from, vertex_to, weight;
-            cin >> vertex_from >> vertex_to >> weight;
-            vertices[vertex_from].edges.push(Edge(vertex_to, weight));
-
-            // TODO: Won't work here, cold fix later but probably no use
-            if (print) {
-                Edge last = vertices[vertex_from].edges.top();
-                cout << vertex_from << " " << last.vertex << " " << last.weight << endl;
-            }
-        }
-    }
-
-    // Computes the shortest distance to vertices in the graph using Prim's Algorithm
-    void prim() {
-        Vertex& start = vertices[0];
-        start.distance = 0;
-//        priority_queue<Vertex, vector<Vertex>, CompareDistances> pq;
-//        pq.push(start);
-
-        vector<Vertex> tree;
-        tree.push_back()
-
-        while (tree.size() == vertices.size()) {
-
-        }
-
-
-//        while (tree.size() == vertices.size()) {
-//            // TODO: Fix this, had problems with references, solved with sketchy id system so far
-//            long long vertex_id = pq.top().id;
-//            pq.pop();
-//            Vertex& vertex = vertices[vertex_id];
-//
-//            for (Edge edge: vertex.edges) {
-//                long long new_distance = vertex.distance + edge.weight;
-//                Vertex& adjacent_vertex = vertices[edge.vertex];
-//
-//                if (new_distance < adjacent_vertex.distance) {
-//                    pq.push(adjacent_vertex);
-//                    adjacent_vertex.distance = new_distance;
-//                }
-//            }
-//        }
-    }
-
-    // Prints out distances from queries to vertices, beginning with starting vertex.
-    // Prints "Impossible" if the distance cannot be reached, otherwise prints the distance to the vertex.
-    void print_distances() {
-//        for (long long query: queries) {
-//            long long distance = vertices[query].distance;
-//            if (distance == LLONG_MAX) {
-//                cout << "Impossible" << endl;
-//            } else {
-//                cout << distance << endl;
-//            }
-//        }
-    }
-};
-
-// Runs Prim's algorithm for all graphs
-void run_prim_multiple_graphs(vector<Graph>& graphs) {
-    for (Graph& graph: graphs) {
-        graph.prim();
-    }
-}
-
-// Prints out distances for all graphs
-void print_distances_multiple_graphs(const vector<Graph>& graphs) {
-    for (int i = 0; i < graphs.size(); i++) {
-        Graph graph = graphs[i];
-        graph.print_distances();
-
-        // TODO: Fix this
-        if (i < graphs.size() - 1) {
-            cout << endl;
-        }
-    }
-}
-
-// Constructs graphs based on input from cin
-vector<Graph> get_graphs_from_input(bool print=false) {
-    vector<Graph> graphs;
-
-    while (true) {
-        long long vertices_amount, edges;
-        cin >> vertices_amount >> edges;
-        if (vertices_amount == 0 and edges == 0) {
-            break;
-        }
-        Graph graph = Graph(vertices_amount);
-        graph.add_edges_from_input(edges, print);
-        graphs.push_back(graph);
-    }
-
-    return graphs;
-}
+//int graph[4][4] = {
+//        {0,  9,  75, 0},
+//        {9,  0,  95, 19},
+//        {75, 95, 0,  51},
+//        {0,  19, 51, 0},
+//        };
 
 int main() {
-    vector<Graph> graphs = get_graphs_from_input(false);
-    run_prim_multiple_graphs(graphs);
-    print_distances_multiple_graphs(graphs);
+    int vertices_amount, edges_amount;
+    cin >> vertices_amount >> edges_amount;
+
+    while (!(vertices_amount == 0 and edges_amount == 0)) {
+        // Getting the input, storing it to graph
+        int graph[vertices_amount][vertices_amount];
+
+        // Initialize graph with positive infinity
+        for (int y = 0; y < vertices_amount; y++) {
+            for (int x = 0; x < vertices_amount; x++) {
+                graph[y][x] = 0;
+            }
+        }
+
+        // Add edges
+        for (int edge = 0; edge < edges_amount; edge++) {
+            int from, to, weight;
+            cin >> from >> to >> weight;
+            graph[from][to] = weight;
+            graph[to][from] = weight;
+        }
+
+        // Sets the diagonal zeros
+        // Should be here because the problem sets some graph[i][i] values to not be 0, while expecting 0
+        for (int vertex = 0; vertex < vertices_amount; vertex++) {
+            graph[vertex][vertex] = 0;
+        }
+
+        // Prim's Algorithm
+        int no_edge;  // number of edge
+
+        // create an array to track selected vertex
+        // selected will become true otherwise false
+        int selected[vertices_amount];
+
+        // set selected false initially
+        memset(selected, false, sizeof(selected));
+
+        // set number of edge to 0
+        no_edge = 0;
+
+        // the number of edges in minimum spanning tree will be
+        // always less than (V -1), where V is number of vertices in
+        //graph
+
+        // choose 0th vertex and make it true
+        selected[0] = true;
+
+        int x;  //  row number
+        int y;  //  col number
+
+        // print for edge and weight
+        cout << "Edge"
+             << " : "
+             << "Weight";
+        cout << endl;
+        while (no_edge < vertices_amount - 1) {
+            //For every vertex in the set S, find the all adjacent vertices
+            // , calculate the distance from the vertex selected at step 1.
+            // if the vertex is already in the set S, discard it otherwise
+            //choose another vertex nearest to selected vertex  at step 1.
+
+            int min = INF;
+            x = 0;
+            y = 0;
+
+            for (int i = 0; i < vertices_amount; i++) {
+                if (selected[i]) {
+                    for (int j = 0; j < vertices_amount; j++) {
+                        if (!selected[j] && graph[i][j]) {  // not in selected and there is an edge
+                            if (min > graph[i][j]) {
+                                min = graph[i][j];
+                                x = i;
+                                y = j;
+                            }
+                        }
+                    }
+                }
+            }
+            cout << x << " - " << y << " :  " << graph[x][y];
+            cout << endl;
+            selected[y] = true;
+            no_edge++;
+        }
+
+        cin >> vertices_amount >> edges_amount;
+    }
+
 
     return 0;
 }
